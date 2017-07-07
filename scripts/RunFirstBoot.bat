3ab847b9d1cadc6e7dbcc75dd441740fc3c7a98e
hugsy <hugsy@blah.cat>
2017-07-07 13:06:50 -0700

    RunFirstBoot.bat : more powershell tweaking

--- scripts/RunFirstBoot.bat
+++ scripts/RunFirstBoot.bat
@@ -11,16 +11,18 @@ netsh advfirewall firewall set rule group="remote administration" new enable=yes
 netsh advfirewall firewall add rule name="Open Port 5985" dir=in action=allow protocol=TCP localport=5985
 netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
 
-winrm quickconfig -q
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Service\AllowUnencrypted -Value True; Set-Item WSMan:\localhost\Service\Auth\Basic -Value True"
-winrm quickconfig -transport:http
-winrm set winrm/config '@{MaxTimeoutms="7200000"}'
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="0"}'
-winrm set winrm/config/winrs '@{MaxProcessesPerShell="0"}'
-winrm set winrm/config/winrs '@{MaxShellsPerUser="0"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/client/auth '@{Basic="true"}'
+call winrm quickconfig -q
+call winrm quickconfig -transport:http
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Enable-PSRemoting -Force"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\MaxTimeoutms 1800000"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxMemoryPerShellMB 0"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxProcessesPerShell 0"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxShellsPerUser 0"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Service\AllowUnencrypted -Value True"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Service\Auth\Basic -Value True"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Client\Auth\Basic -Value True"
+
 net stop winrm
 sc triggerinfo winrm start/networkon stop/networkoff
 sc.exe config "WinRM" start= auto
