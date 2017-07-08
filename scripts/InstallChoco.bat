05f4a2631d2e22e6b839cc112207b158c38ae3d0
hugsy <hugsy@blah.cat>
2017-07-07 17:46:59 -0700

    reverting scripts/InstallApps.bat and scripts/InstallChoco.bat

---
+++ scripts/InstallChoco.bat
@@ -0,0 +1,1 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
