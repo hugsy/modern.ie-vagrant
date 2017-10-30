b7447fcb7ecea75e17a8f63aa578afae13969c2f
crazy rabbidz <hugsy@users.noreply.github.com>
2017-10-30 00:11:47 -0700

    Update InstallApps.bat

--- scripts/InstallApps.bat
+++ scripts/InstallApps.bat
@@ -4,7 +4,7 @@ C:\ProgramData\Chocolatey\choco.exe install -y --force firefox
 C:\ProgramData\Chocolatey\choco.exe install -y --force notepadplusplus.install
 C:\ProgramData\Chocolatey\choco.exe install -y --force conemu
 C:\ProgramData\Chocolatey\choco.exe install -y --force irfanview
-C:\ProgramData\Chocolatey\choco.exe install -y --force sumatrapdf
+C:\ProgramData\Chocolatey\choco.exe install -y --force sumatrapdf sumatrapdf.install
 C:\ProgramData\Chocolatey\choco.exe install -y --force windbg
 C:\ProgramData\Chocolatey\choco.exe install -y --force x64dbg.portable
 C:\ProgramData\Chocolatey\choco.exe install -y --force HxD
