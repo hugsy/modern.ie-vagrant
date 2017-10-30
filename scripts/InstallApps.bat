c590cce899d4f9af866eaec9851fbd33bcf4ceac
crazy rabbidz <hugsy@users.noreply.github.com>
2017-10-30 00:08:12 -0700

    Update InstallApps.bat

--- scripts/InstallApps.bat
+++ scripts/InstallApps.bat
@@ -1,4 +1,5 @@
 C:\ProgramData\Chocolatey\choco.exe install -y --force GoogleChrome
+C:\ProgramData\Chocolatey\choco.exe install -y --force SysInternals
 C:\ProgramData\Chocolatey\choco.exe install -y --force firefox
 C:\ProgramData\Chocolatey\choco.exe install -y --force notepadplusplus.install
 C:\ProgramData\Chocolatey\choco.exe install -y --force conemu
