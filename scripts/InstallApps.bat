13b5cbbffed653718655a63ab57a666139a63dd7
crazy rabbidz <hugsy@users.noreply.github.com>
2017-11-16 17:22:30 -0800

    Update InstallApps.bat

--- scripts/InstallApps.bat
+++ scripts/InstallApps.bat
@@ -7,6 +7,5 @@ C:\ProgramData\Chocolatey\choco.exe install -y --force notepadplusplus.install
 C:\ProgramData\Chocolatey\choco.exe install -y --force irfanview
 C:\ProgramData\Chocolatey\choco.exe install -y --force sumatrapdf sumatrapdf.install
 C:\ProgramData\Chocolatey\choco.exe install -y --force windbg
-C:\ProgramData\Chocolatey\choco.exe install -y --force x64dbg.portable
 C:\ProgramData\Chocolatey\choco.exe install -y --force HxD
 C:\ProgramData\Chocolatey\choco.exe install -y --force 7zip.install
