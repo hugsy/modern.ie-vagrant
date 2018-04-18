f1be6fcc5f7f2d50a1340da1ac67db1bed08e8e4
crazy rabbidz <hugsy@users.noreply.github.com>
2018-04-17 18:19:49 -0700

    Added fzf to the list of tools to install via choco

--- scripts/InstallApps.bat
+++ scripts/InstallApps.bat
@@ -10,3 +10,4 @@ C:\ProgramData\Chocolatey\choco.exe install -y --force windbg
 C:\ProgramData\Chocolatey\choco.exe install -y --force HxD
 C:\ProgramData\Chocolatey\choco.exe install -y --force doublecmd
 C:\ProgramData\Chocolatey\choco.exe install -y --force 7zip.install
+C:\ProgramData\Chocolatey\choco.exe install -y --force fzf
