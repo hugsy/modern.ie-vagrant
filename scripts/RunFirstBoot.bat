22e96ac576069210b4aefdd7f6a10a23c73ec38b
crazy rabbidz <hugsy@users.noreply.github.com>
2018-08-27 07:37:31 -0700

    Update RunFirstBoot.bat

--- scripts/RunFirstBoot.bat
+++ scripts/RunFirstBoot.bat
@@ -1,10 +1,13 @@
-@xcopy "\\vboxsrv\vagrant\scripts\wallpaper.jpg" "C:\Users\IEUser\" /y
-@xcopy "\\vboxsrv\vagrant\scripts\SetWallPaper.ps1" "C:\Users\IEUser\" /y
-@xcopy "\\vboxsrv\vagrant\scripts\SetWallPaper.bat" "C:\Users\IEUser\" /y
-@xcopy "\\vboxsrv\vagrant\scripts\SetWallpaper.bat" "C:\Users\IEUser\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\" /y
-@reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v SetWallpaper /d "C:\Users\IEUser\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\SetWallpaper.bat" /f
+@echo off
+xcopy "\\vboxsrv\vagrant\scripts\wallpaper.jpg" "C:\Users\IEUser\" /y
+xcopy "\\vboxsrv\vagrant\scripts\SetWallPaper.ps1" "C:\Users\IEUser\" /y
+xcopy "\\vboxsrv\vagrant\scripts\SetWallPaper.bat" "C:\Users\IEUser\" /y
+xcopy "\\vboxsrv\vagrant\scripts\SetWallpaper.bat" "C:\Users\IEUser\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\" /y
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v SetWallpaper /d "C:\Users\IEUser\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\SetWallpaper.bat" /f
 
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-NetConnectionProfile -NetworkCategory Private"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-NetConnectionProfile -NetworkCategory Private"
+
+rd /S /Q C:\BGInfo
 
 netsh advfirewall firewall set rule group="remote administration" new enable=yes
 netsh advfirewall firewall add rule name="Open Port 5985" dir=in action=allow protocol=TCP localport=5985
@@ -19,14 +22,15 @@ net start termservice /y
 call winrm quickconfig -q
 call winrm quickconfig -transport:http
 
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Enable-PSRemoting -Force"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\MaxTimeoutms 1800000"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxMemoryPerShellMB 0"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxProcessesPerShell 0"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxShellsPerUser 0"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Service\AllowUnencrypted -Value True"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Service\Auth\Basic -Value True"
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Client\Auth\Basic -Value True"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Enable-PSRemoting -Force"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\MaxTimeoutms 1800000"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxMemoryPerShellMB 0"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxProcessesPerShell 0"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\WinRS\MaxShellsPerUser 0"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Service\AllowUnencrypted -Value True"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Service\Auth\Basic -Value True"
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Item WSMan:\localhost\Client\Auth\Basic -Value True"
 
 net stop winrm /y
 sc triggerinfo winrm start/networkon stop/networkoff
