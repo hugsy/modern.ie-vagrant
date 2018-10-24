5e5bb2b4f034272fa8a229730ebf35989a86df54
crazy rabbidz <hugsy@users.noreply.github.com>
2018-10-24 15:01:10 -0700

    Update InstallApps.bat

--- scripts/InstallApps.bat
+++ scripts/InstallApps.bat
@@ -1,17 +1,17 @@
-C:\ProgramData\Chocolatey\choco.exe install -y --force ConEmu
-C:\ProgramData\Chocolatey\choco.exe install -y --force GoogleChrome
-C:\ProgramData\Chocolatey\choco.exe install -y --force SysInternals
-C:\ProgramData\Chocolatey\choco.exe install -y --force processhacker
-C:\ProgramData\Chocolatey\choco.exe install -y --force firefox
-C:\ProgramData\Chocolatey\choco.exe install -y --force notepadplusplus.install
-C:\ProgramData\Chocolatey\choco.exe install -y --force irfanview
-C:\ProgramData\Chocolatey\choco.exe install -y --force sumatrapdf sumatrapdf.install
-C:\ProgramData\Chocolatey\choco.exe install -y --force windbg
-REM C:\ProgramData\Chocolatey\choco.exe install -y --force windowsdriverkit10
-C:\ProgramData\Chocolatey\choco.exe install -y --force HxD
-C:\ProgramData\Chocolatey\choco.exe install -y --force explorersuite
-C:\ProgramData\Chocolatey\choco.exe install -y --force 7zip.install
-C:\ProgramData\Chocolatey\choco.exe install -y --force apimonitor
-C:\ProgramData\Chocolatey\choco.exe install -y --force python2
-C:\ProgramData\Chocolatey\choco.exe install -y --force resourcehacker.portable
-
+C:\ProgramData\Chocolatey\choco.exe install -y 7zip.install
+C:\ProgramData\Chocolatey\choco.exe install -y python2
+C:\ProgramData\Chocolatey\choco.exe install -y ConEmu
+C:\ProgramData\Chocolatey\choco.exe install -y GoogleChrome
+C:\ProgramData\Chocolatey\choco.exe install -y SysInternals
+C:\ProgramData\Chocolatey\choco.exe install -y processhacker
+C:\ProgramData\Chocolatey\choco.exe install -y firefox
+C:\ProgramData\Chocolatey\choco.exe install -y notepadplusplus.install
+C:\ProgramData\Chocolatey\choco.exe install -y irfanview
+C:\ProgramData\Chocolatey\choco.exe install -y sumatrapdf sumatrapdf.install
+C:\ProgramData\Chocolatey\choco.exe install -y windbg
+REM C:\ProgramData\Chocolatey\choco.exe install -y windowsdriverkit10
+C:\ProgramData\Chocolatey\choco.exe install -y HxD
+C:\ProgramData\Chocolatey\choco.exe install -y explorersuite
+C:\ProgramData\Chocolatey\choco.exe install -y ida-free
+C:\ProgramData\Chocolatey\choco.exe install -y apimonitor
+C:\ProgramData\Chocolatey\choco.exe install -y --ignore-checksums resourcehacker.portable
