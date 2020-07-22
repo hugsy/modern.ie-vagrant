43baeda4aa899b6fd84630ef0c75a6c9d3d17044
Daniel Pendolino <daniel@counterhack.com>
2020-07-22 13:17:47 -0400

    clean up code to grab the latest box url from Microsoft

--- get-modern-ie-box.sh
+++ get-modern-ie-box.sh
@@ -7,7 +7,7 @@ if [ ! -f MSEdge-Win10.box ]; then
 
   while [ -z "$box_url" ]; do
     echo "Attempting to get latest box url from Microsoft.."
-    box_url=$(google-chrome-stable --headless --disable-gpu --dump-dom https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/ | tr -d '\n' | egrep -o "http[^\'\"]*VirtualBox.zip")
+    box_url=$(google-chrome-stable --virtual-time-budget=10000 --headless --disable-gpu --dump-dom https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/ | tr -d '\n' | egrep -o "http[^\'\"]*Vagrant.zip")
   done
 
   echo "Box url is $box_url"
